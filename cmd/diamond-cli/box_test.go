3e42fc519899d28cca4b8d2316234a3615abe3c4
nvm <s.avkhadeev@gmail.com>
2024-01-16 20:46:35 +0600

    update box test

--- cmd/diamond-cli/box_test.go
+++ cmd/diamond-cli/box_test.go
@@ -1,45 +1,14 @@
 package main
 
 import (
-	"context"
-	"crypto/ecdsa"
-	"errors"
 	"math/big"
 	"reflect"
 	"strings"
 	"testing"
 
 	"github.com/ethereum/go-ethereum/accounts/abi"
-	"github.com/ethereum/go-ethereum/accounts/abi/bind"
-	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
 )
-
-type MockEthereumWrapper struct {
-	mock.Mock
-}
-
-func (m *MockEthereumWrapper) Dial(rawurl string) (*ethclient.Client, error) {
-	args := m.Called(rawurl)
-	return args.Get(0).(*ethclient.Client), args.Error(1)
-}
-
-func (m *MockEthereumWrapper) NewKeyedTransactorWithChainID(key *ecdsa.PrivateKey, chainID *big.Int) (*bind.TransactOpts, error) {
-	return nil, nil
-}
-
-func (m *MockEthereumWrapper) NetworkID(ctx context.Context) (*big.Int, error) {
-	return big.NewInt(1), nil
-}
-
-func (m *MockEthereumWrapper) SuggestGasPrice(ctx context.Context) (*big.Int, error) {
-	return big.NewInt(21000), nil
-}
-
-func (m *MockEthereumWrapper) HexToECDSA(hexkey string) (*ecdsa.PrivateKey, error) {
-	return nil, errors.New("HexToECDSA failed")
-}
 
 func TestNewDiamondBox(t *testing.T) {
 	// config := Config{
