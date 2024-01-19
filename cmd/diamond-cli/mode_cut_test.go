3c796f5c0d0fa12743ef2c6074fa25394ef6ff56
nvm <s.avkhadeev@gmail.com>
2024-01-19 17:46:54 +0600

    add cut mode tests

--- cmd/diamond-cli/mode_cut_test.go
+++ cmd/diamond-cli/mode_cut_test.go
@@ -3,8 +3,10 @@ package main
 import (
 	"testing"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/spf13/pflag"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
 func TestNewCutMode(t *testing.T) {
@@ -51,14 +53,79 @@ func TestCutMode_Execute(t *testing.T) {
 		t.Fatalf("Expected type CutMode, got %T", mode)
 	}
 
-	cutMode.cutContract = &MockBoundContract{}
+	mockContract := setupMockCutContract()
 
+	cutMode.cutContract = mockContract
 	cmd := &Command{Name: "add"}
 	flags := pflag.NewFlagSet("test", pflag.ContinueOnError)
 	flags.String("address", "0xCAFEBABECAFEBABECAFEBABECAFEBABECAFEBABE", "")
 	flags.String("selectors", "0xdeadbeef", "")
 
-	err = cutMode.Execute(cmd, flags)
+	var cut []FacetCut
+	var facetAddress AddressFlag
+	var functionSelectors SelectorFlag
 
+	calldata, err := box.contracts["diamond_init"].ABI.Pack("init")
 	assert.Nil(t, err)
+	
+	addressString, err := flags.GetString("address")
+	assert.Nil(t, err)
+
+	err = facetAddress.Set(addressString)
+	assert.Nil(t, err)
+
+	selectorString, err := flags.GetString("selectors")
+	assert.Nil(t, err)
+
+	err = functionSelectors.Set(selectorString)
+	assert.Nil(t, err)
+
+	cut = append(cut, FacetCut{
+		FacetAddress:      common.Address(facetAddress),
+		Action:            Add,
+		FunctionSelectors: functionSelectors,
+	})
+
+	err = cutMode.Execute(cmd, flags)
+	mockContract.AssertCalled(t, "Transact", mock.Anything, "diamondCut",
+		cut,
+		box.config.Contracts["diamond_init"].Address,
+		calldata)
+	assert.Nil(t, err)
+}
+
+func TestCutMode_Execute_InvalidAddress(t *testing.T) {
+	box, err := setupBox()
+	if err != nil {
+		t.Fatalf("Failed to create DiamondBox: %v", err)
+	}
+
+	mode := NewCutMode(box)
+	cmd := &Command{Name: "add"}
+	flags := pflag.NewFlagSet("test", pflag.ContinueOnError)
+	flags.String("address", "invalid", "")
+	flags.String("selectors", "0x00000000", "")
+
+	err = mode.Execute(cmd, flags)
+
+	assert.NotNil(t, err)
+	assert.Contains(t, err.Error(), "invalid Ethereum address format")
+}
+
+func TestCutMode_Execute_InvalidSelectors(t *testing.T) {
+	box, err := setupBox()
+	if err != nil {
+		t.Fatalf("Failed to create DiamondBox: %v", err)
+	}
+
+	mode := NewCutMode(box)
+	cmd := &Command{Name: "add"}
+	flags := pflag.NewFlagSet("test", pflag.ContinueOnError)
+	flags.String("address", "0xCAFEBABECAFEBABECAFEBABECAFEBABECAFEBABE", "")
+	flags.String("selectors", "invalid", "")
+
+	err = mode.Execute(cmd, flags)
+
+	assert.NotNil(t, err)
+	assert.Contains(t, err.Error(), "invalid selector format")
 }
